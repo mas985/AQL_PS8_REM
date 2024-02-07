6666eaae13e3625cead072ced370aa1042aa816b
mas985 <mas985@gmail.com>
2024-02-07 08:37:20 -0800

    v1.1.3 GUI Update Fix

--- AquaLogicPS8/MainPage.xaml.cs
+++ AquaLogicPS8/MainPage.xaml.cs
@@ -48,8 +48,8 @@ namespace AquaLogicPS8
             // Make panel display as large as possible
 
 #if WINDOWS
-            double dispHeight = 840;
-            double dispWidth = 420;
+            double dispHeight = 760;
+            double dispWidth = 400;
 #else
             double dispHeight = DeviceDisplay.Current.MainDisplayInfo.Height / DeviceDisplay.Current.MainDisplayInfo.Density;
             double dispWidth = DeviceDisplay.Current.MainDisplayInfo.Width / DeviceDisplay.Current.MainDisplayInfo.Density;
@@ -64,11 +64,11 @@ namespace AquaLogicPS8
             }
 
             double tdWidth = dispWidth - GRID1.Margin.HorizontalThickness - TextDisplay.Margin.HorizontalThickness;
-            double tdHeight = Math.Max(TextDisplay.FontSize * 3, Math.Min(tdWidth / 10 * 3, dispHeight * 0.88 - GRID1.Margin.VerticalThickness -
-              (Aux1.HeightRequest + Aux1.Margin.VerticalThickness) * 10 - TextDisplay.Margin.VerticalThickness - 5));
+            double tdHeight = Math.Min(tdWidth / 12 * 3, Math.Max(TextDisplay.FontSize * 3, dispHeight * 0.85 - GRID1.Margin.VerticalThickness -
+              (Aux1.HeightRequest + Aux1.Margin.VerticalThickness) * 10 - TextDisplayBorder.Margin.VerticalThickness));
 
             TextDisplay.HeightRequest = tdHeight;
-            TextDisplay.FontSize = tdHeight / 3;
+            TextDisplay.FontSize = Math.Min(tdWidth / 12, tdHeight / 3);
         }
 
         string _ipAddr;
